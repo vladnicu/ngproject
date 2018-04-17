48d84ece5515e2d9a188df062b77b6bd3a72f691
Iringo <iringo.reka@yahoo.com>
2018-04-17 07:50:02 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from './note';
+import { NOTES } from './mock-notes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  notesCount: number;
+
+  notes = NOTES;
+
+  selectedNote = NOTES[0];
+
+  onSelect(note: Note): void {
+    this.selectedNote = note;
+  }
+
+  ngOnInit(){
+    this.notesCount = this.notes.length;
+  }
+
+  addItem(){
+    var note: Note = {
+      title: this.selectedNote.title,
+      text: this.selectedNote.text
+    };
+    
+    this.notes.push(note);
+    this.selectedNote = note;
+  }
+
+  removeItem(i){
+    this.notes.splice(i, 1);
+    this.notesCount = this.notes.length;
+  } 
 }
---
+++ src/app/mock-notes.ts
@@ -0,0 +1,7 @@
+import { Note } from './note';
+
+export const NOTES: Note[] = [
+  { title: 'First note', text: 'lorem impsum tralala' },
+  { title: 'Ski trip', text: 'tes test test' },
+  { title: 'Learn Angular', text: 'la lala' }
+];
